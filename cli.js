4dd25f160d01f28dacdacba171bcd274967919f6
Jae Chung <jaechung77@gmail.com>
2022-06-23 22:08:43 +1000

    add table format

--- cli.js
+++ cli.js
@@ -1,11 +1,18 @@
 #! /usr/bin/end mode
 
 const axios = require('axios');
+const Table = require('cli-table');
 // const API = 'https://api.publicapis.org/entries';
-const file = 'data.json';
-const category_input = process.argv[2];
-const limit = process.argv[3];
+
+const categoryInput = process.argv[2];
+const noOfData = process.argv[3];
+
 const fs = require('fs');
+
+const table = new Table({
+  head: ['API', 'Description', 'Auth', 'HTTPS', 'Cors', 'Link', 'Category'],
+  colWidths: [20, 80, 10, 10, 10, 80, 20],
+});
 
 console.log('Loading...');
 
@@ -26,7 +33,20 @@ const getAllEntries = () => {
     .sort((a, b) => b.API.localeCompare(a.API))
     .slice(0, 5);
   console.clear();
-  console.log(output);
+
+  output.map((row) =>
+    table.push([
+      row.API,
+      row.Description,
+      row.Auth,
+      row.HTTPS,
+      row.Cors,
+      row.Link,
+      row.Category,
+    ])
+  );
+  console.log(`\n Category: ${categoryInput}, No. of Data: ${noOfData}\n\n`);
+  console.log(table.toString());
 };
 
 getAllEntries();
